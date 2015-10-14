03a4ed441f300600f16481e27d9877312faa1c58
aoyamakohei <aoyamakohei@team-lab.com>
2015-10-14 15:24:34 +0900

    サンプルコードを追加

--- Assets/XmlSaver/Example/Example.cs
+++ Assets/XmlSaver/Example/Example.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
-public class Example : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+namespace XmlSaver {
+    public class Example : MonoBehaviour {
+        public class AAA {
+            public string str = "string-AAA";
+            public List<string> list = new List<string>() {
+                "A", "B", "C"
+            };
+        }
+
+        
+        void Start() {
+            XmlSaver.SetInt("integer", 10);
+            XmlSaver.SetFloat("float", 1.1f);
+            XmlSaver.Set<AAA>("aaa", new AAA());
+
+            XmlSaver.Save();
+
+            Debug.Log(XmlSaver.GetInt("integer"));
+            Debug.Log(XmlSaver.GetFloat("float"));
+            Debug.Log(XmlSaver.GetBool("bool", false));
+            Debug.Log(XmlSaver.Get<AAA>("aaa", null).list.First());
+        }
+    }
 }
