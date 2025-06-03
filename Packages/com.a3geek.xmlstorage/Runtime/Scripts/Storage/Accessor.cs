fb8ef23c71ec044222f5dc0d5d1a1027fd973c6d
a3geek <a3geek@gmail.com>
2025-06-03 16:12:22 +0900

    外部公開のメソッドに引数増やすの忘れてた

--- Packages/com.a3geek.xmlstorage/Runtime/Scripts/Storage/Accessor.cs
+++ Packages/com.a3geek.xmlstorage/Runtime/Scripts/Storage/Accessor.cs
@@ -23,29 +23,29 @@ namespace XmlStorage
         }
 
     #region "Setter"
-        public static void Set<T>(string key, T value)
+        public static void Set<T>(string key, T value, string groupName = null)
         {
-            SetValue(key, value, typeof(T));
+            SetValue(key, value, typeof(T), groupName);
         }
 
-        public static void SetInt(string key, int value)
+        public static void SetInt(string key, int value, string groupName = null)
         {
-            SetValue(key, value, typeof(int));
+            SetValue(key, value, typeof(int), groupName);
         }
 
-        public static void SetFloat(string key, float value)
+        public static void SetFloat(string key, float value, string groupName = null)
         {
-            SetValue(key, value, typeof(float));
+            SetValue(key, value, typeof(float), groupName);
         }
 
-        public static void SetBool(string key, bool value)
+        public static void SetBool(string key, bool value, string groupName = null)
         {
-            SetValue(key, value, typeof(bool));
+            SetValue(key, value, typeof(bool), groupName);
         }
 
-        public static void SetString(string key, string value)
+        public static void SetString(string key, string value, string groupName = null)
         {
-            SetValue(key, value, typeof(string));
+            SetValue(key, value, typeof(string), groupName);
         }
 
         private static void SetValue<T>(string key, T value, Type valueType, string groupName = null)
@@ -56,32 +56,32 @@ namespace XmlStorage
     #endregion
 
     #region "Getter"
-        public static T Get<T>(string key, T defaultValue = default)
+        public static T Get<T>(string key, T defaultValue = default, string groupName = null)
         {
-            return GetValue(key, defaultValue, typeof(T));
+            return GetValue(key, defaultValue, typeof(T), groupName);
         }
 
-        public static int GetInt(string key, int defaultValue = 0)
+        public static int GetInt(string key, int defaultValue = 0, string groupName = null)
         {
-            return GetValue(key, defaultValue, typeof(int));
+            return GetValue(key, defaultValue, typeof(int), groupName);
         }
 
-        public static float GetFloat(string key, float defaultValue = 0f)
+        public static float GetFloat(string key, float defaultValue = 0f, string groupName = null)
         {
-            return GetValue(key, defaultValue, typeof(float));
+            return GetValue(key, defaultValue, typeof(float), groupName);
         }
 
-        public static bool GetBool(string key, bool defaultValue = false)
+        public static bool GetBool(string key, bool defaultValue = false, string groupName = null)
         {
-            return GetValue(key, defaultValue, typeof(bool));
+            return GetValue(key, defaultValue, typeof(bool), groupName);
         }
 
-        public static string GetString(string key, string defaultValue = "")
+        public static string GetString(string key, string defaultValue = "", string groupName = null)
         {
-            return GetValue(key, defaultValue, typeof(string));
+            return GetValue(key, defaultValue, typeof(string), groupName);
         }
 
-        private static T GetValue<T>(string key, T defaultValue,  Type valueType, string groupName = null)
+        private static T GetValue<T>(string key, T defaultValue, Type valueType, string groupName = null)
         {
             var group = GetCurrentDataGroup(groupName ?? CurrentDataGroupName);
             if (!group.GetData().TryGet(key, valueType, out var data))
